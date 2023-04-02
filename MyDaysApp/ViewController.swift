48b412fc94eaadf13d0cc1780a45f5ae09097dac
Vsevolod <vsevolod.lashin@gmail.com>
2023-04-02 18:45:13 +0300

    resultButtonTapped function logic added

--- MyDaysApp/ViewController.swift
+++ MyDaysApp/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var daysLabel: UILabel!
+    @IBOutlet var resultButton: UIButton!
+    @IBOutlet var birthdayDP: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        resultButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func resultButtonTapped() {
+        guard birthdayDP.date < Date.now else { return }
+        
+        let range = birthdayDP.date..<Date.now
+        let numberOfDays = range.formatted(.components(style: .wide, fields: [.day]))
+        daysLabel.text = "Ты наслаждаешься жизнью уже \(numberOfDays)"
+    }
+    
 }
 
